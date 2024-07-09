eca0a67fe68f0e369d2a6dc4387bee80750cfda6
frank <kiol726as@gmail.com>
2024-07-09 16:53:17 +0800

    bs4 adding

--- python_learning_day1.py
+++ python_learning_day1.py
@@ -4,8 +4,8 @@ import bs4
 #python.exe -m pip list 可以查看python 目前的模組
 res=requests.get("https://www.google.com/search?client=firefox-b-d&q=google")
 
-
+soup=bs4.BeautifulSoup(res.text)
 print(res)
-
+print(soup)
 print("hello world")
 print("123")
