b5bcd306770d103a95e05a50c2a0b1f18f69f612
frank <kiol726as@gmail.com>
2024-07-09 16:44:24 +0800

    Update python_learning_day1.py

--- python_learning_day1.py
+++ python_learning_day1.py
@@ -1,8 +1,11 @@
 import requests
 import bs4
+
+#python.exe -m pip list 可以查看python 目前的模組
 res=requests.get("https://www.google.com/search?client=firefox-b-d&q=google")
 
 
 print(res)
 
 print("hello world")
+print("123")
